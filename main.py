f452fa96552044aa6e0526405e17a7c269e1e893
Hank O'Brien <hank.obrien900@icloud.com>
2018-02-22 13:57:32 -0800

    added slackbot code (arsh wrote) and license

--- main.py
+++ main.py
@@ -61,6 +61,56 @@ def generate_article():
         art_text = art_text.decode('utf8')
         return render_template("home.html", title=tit_text, article=art_text)
 
+
+@app.route('/post', methods=["POST","GET"])
+def generate_article_slack():
+    keywords = request.form.get("text")
+    if keywords == None:
+        return render_template("home.html")
+    else:
+        keywords = keywords.split(" ")
+        kwords = []
+        for word in keywords:
+            kwords.append(word.lower())
+
+        articles = []
+        for file in os.listdir("articles/"):
+            if file.endswith(".txt"):
+                text = open(os.path.join("articles/", file), "r").read()
+                source = file[:file.index("-")]
+                articles.append(Article(text, source))
+        weighted_articles = []
+        for art in articles:
+            weighted_articles.append((similarity(art.vector, keywords), art))
+        weighted_articles = sorted(weighted_articles, key=lambda x: -x[0])
+        temp = []
+        for pair in weighted_articles:
+            if pair[0] > 0:
+                temp.append(pair)
+        weighted_articles = temp
+        if len(weighted_articles) >= 3:
+            model = weighted_articles[0:3]
+        else:
+            model = weighted_articles
+        articles = []
+        for pair in model:
+            art = pair[1]
+            articles.append(art)
+        generated_article, sources = group_sentences(articles)
+        title = ""
+        art_text = ""
+        for sentence in generated_article:
+            art_text += sentence[0] + " "
+        if len(generated_article) > 0:
+            title = create_title(art_text)
+        else:
+            title = "Sorry, we couldn't find any related articles!"
+        #generate the text and display some how
+        tit_text = title.decode('utf8')
+        art_text = art_text.decode('utf8')
+        return tit_text + ": "+ art_text
+
+
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=8080, debug=True)
 
